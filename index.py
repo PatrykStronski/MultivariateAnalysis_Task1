1d2e24a6058853e86708fac9174b3e086d855db9
Patryk Stronski <p.stronski13@gmail.com>
2021-10-07 20:55:04 +0200

    experimental checks

--- index.py
+++ index.py
@@ -14,24 +14,18 @@ def draw_diagrams(data_frame: pd.DataFrame):
     plt.plot(data_frame.x, data_frame.y)
     plt.show()
 
-def get_moments(data_frame: pd.DataFrame):
+def print_moments(data_frame: pd.DataFrame):
     mean_x = data_frame.x.mean()
     var_x = data_frame.x.var()
+    print(f"mean_x: {mean_x}, var_x: {var_x}")
 
     mean_y = data_frame.y.mean()
     var_y = data_frame.y.var()
+    print(f"mean_y: {mean_y}, var_x: {var_y}")
 
     mean_s = data_frame.s.mean()
     var_s = data_frame.s.var()
-
-    return {
-        mean_x,
-        mean_y,
-        mean_s,
-        var_x,
-        var_y,
-        var_s
-    }
+    print(f"mean_s: {mean_s}, var_s: {var_s}")
 
 
 pd.set_option('display.notebook_repr_html', False)
@@ -39,7 +33,6 @@ pd.set_option('display.max_columns', 20)
 pd.set_option('display.max_rows', 25)
 
 df = pd.read_csv('./data/tracking2018.csv')
-moments_matrix = pd.DataFrame(columns=['gameId', 'nflId', ])
 
 players = df.nflId.unique()
 games = df.gameId.unique()
@@ -54,4 +47,3 @@ for gameId in games:
 
         previewData = df.loc[(df.nflId == playerId) & (df.gameId == gameId)]
         draw_diagrams(previewData)
-        print_moments(previewData)
