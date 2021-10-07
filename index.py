900daab5760c35afb02198cc0ebf28e861171464
Patryk Stronski <p.stronski13@gmail.com>
2021-10-07 20:52:43 +0200

    experimental checks

--- index.py
+++ index.py
@@ -6,10 +6,32 @@ def draw_diagrams(data_frame: pd.DataFrame):
     plt.hist(data_frame.x, bins=20, label='x-axis')
     plt.hist(data_frame.y, bins=20, label='y-axis')
     plt.hist(data_frame.s, bins=20, label='speed')
+    plt.hist(data_frame.a, bins=20, label='acceleration')
     plt.show()
 
     plt.hist2d(data_frame.x, data_frame.y, bins=80)
     plt.show()
+    plt.plot(data_frame.x, data_frame.y)
+    plt.show()
+
+def get_moments(data_frame: pd.DataFrame):
+    mean_x = data_frame.x.mean()
+    var_x = data_frame.x.var()
+
+    mean_y = data_frame.y.mean()
+    var_y = data_frame.y.var()
+
+    mean_s = data_frame.s.mean()
+    var_s = data_frame.s.var()
+
+    return {
+        mean_x,
+        mean_y,
+        mean_s,
+        var_x,
+        var_y,
+        var_s
+    }
 
 
 pd.set_option('display.notebook_repr_html', False)
@@ -17,6 +39,7 @@ pd.set_option('display.max_columns', 20)
 pd.set_option('display.max_rows', 25)
 
 df = pd.read_csv('./data/tracking2018.csv')
+moments_matrix = pd.DataFrame(columns=['gameId', 'nflId', ])
 
 players = df.nflId.unique()
 games = df.gameId.unique()
@@ -31,3 +54,4 @@ for gameId in games:
 
         previewData = df.loc[(df.nflId == playerId) & (df.gameId == gameId)]
         draw_diagrams(previewData)
+        print_moments(previewData)
